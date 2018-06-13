da818d49a5cd86c8d5c178da5121f93f4ee0a1ca
王湘赣 <wangxianggan@hnjing.com>
2018-06-13 17:52:03 +0800

    a

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const proxy = require('http-proxy-middleware')
+const jwt = require('jsonwebtoken')
 
 const app = express()
 
 
-
+const token = jwt.sign({a: 1}, 't')
+const a = jwt.decode(token, 't')
+console.log(token)
+console.log(a)
 
 app.use(proxy('/cms/v1', {
     target: 'http://127.0.0.1:8000'
