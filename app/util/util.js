a176a0aeb21fd77076a37877b5604e507bd6c538
王湘赣 <wangxianggan@hnjing.com>
2018-07-03 15:36:44 +0800

    a

--- app/util/util.js
+++ app/util/util.js
@@ -9,8 +9,15 @@ function getSecret (key) {
     return JSON.parse(fs.readFileSync(path.join(__dirname, '../../config/secret.json')), 'utf-8')[key]
 }
 
+//获取url中的参数
+function getQueryString (name) {
+    const result = window.location.search.substr(1).match(new RegExp("(^|&)" + name + "=([^&]*)(&|$)", 'i')); // 对querystring匹配目标参数
+    return result ? decodeURIComponent(result[2]) : ''
+}
+
 module.exports = {
     getToken,
     getSecret,
+    getQueryString,
 }
 
